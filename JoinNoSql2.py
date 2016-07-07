4b70656299911cd8d1c322af0f2f51d17a68d378
Mambembe <matteo.giaretti@gmail.com>
2016-07-07 15:26:11 +0200

    hsgdvc

--- JoinNoSql2.py
+++ JoinNoSql2.py
@@ -56,7 +56,7 @@ data_extract2 = table2.map(lambda line: (line.split(','))) \
 
 #both_rdd = data_extract
 
-print '------------------------'+data_extract.count()
+print '------------------------' + str(data_extract.count())
 
 #data_extract.saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
 
