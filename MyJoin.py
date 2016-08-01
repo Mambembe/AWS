b24457bae745d9ee930c2ee72b921a5df16e2446
Mambembe <matteo.giaretti@gmail.com>
2016-08-01 19:08:14 +0200

    sdv

--- MyJoin.py
+++ MyJoin.py
@@ -13,7 +13,7 @@ sys.path.append('/root/spark/python/')
 sc = pyspark.SparkContext()
 
 #table1 = sc.textFile('s3n://bigdives3/DataClean/dbo.shop.header.droppedhard.csv')
-table2 = sc.textFile('s3n://bigdives3/DataClean/DataClean/dbo.shop.STAT_storico_dett.droppedhard.csv')
+table2 = sc.textFile('s3n://bigdives3/DataClean/dbo.shop.STAT_storico_dett.droppedhard.csv')
 
 #table1.take(10).saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
 #print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ecce !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
@@ -31,7 +31,7 @@ def ExtractHeader(table):
 
 
 #table1, header1 = ExtractHeader(table1)
-#table2, header2 = ExtractHeader(table2)
+table2, header2 = ExtractHeader(table2)
 
 def SanityCheck(table, header):
     data_extract = table.map(lambda line: (line.split(','))) \
@@ -41,20 +41,20 @@ def SanityCheck(table, header):
     return data_extract
 
 #table1 = SanityCheck(table1, header1)
-#table2 = SanityCheck(table2)
+table2 = SanityCheck(table2)
 
-print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-print table2.take(6)
+#print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+#print table2.take(6)
 
 
 
 #data_extract = table2.map(lambda line: line[4]+line[5]+line[6])
-#data_extract = table2.map(lambda line: ''.join(re.findall('\d+', line[4]+line[5]+line[6] )))\
-#                    .map(lambda line: line[0:5])
-#aaa = data_extract.filter(lambda line: line == '19782')
+data_extract = table2.map(lambda line: ''.join(re.findall('\d+', line[4]+line[5]+line[6] )))\
+                    .map(lambda line: line[0:5])
+aaa = data_extract.filter(lambda line: line == '19782')
 #aaa = data_extract.filter(lambda line: line == '19782')
 #frequencies = data_extract.map(lambda w: (w, 1)).reduceByKey(lambda v1,v2: v1+v2)
 #print frequencies.take(10)
 
 #data_extract = table1.map(lambda line: line[2])
-#header1.saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
+aaa.saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
