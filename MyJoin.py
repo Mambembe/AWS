0f0339b971484a2da8c63a645c40160cc0ca7d7f
Mambembe <matteo.giaretti@gmail.com>
2016-08-01 18:31:26 +0200

    sdv

--- MyJoin.py
+++ MyJoin.py
@@ -20,37 +20,34 @@ table1 = sc.textFile('s3n://bigdives3/DataClean/dbo.shop.header.droppedhard.csv'
 #data = sc.parallelize([1,2,3,4,5,6])
 #print data
 #print table1.take(10)
-print '!!!!!!!!!!!!!!!!!!!!'
-table1.take(5).saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
-print '!!!!!!!!!!!!!!!!!!!!'
-'''
-def ExtractHeader(_table):
-    temp = _table.first()
-    table = _table.filter(lambda x:x !=temp).cache()
+
+def ExtractHeader(table):
+    temp = table.first()
+    table = table.filter(lambda x:x !=temp)
     header = temp.split(',')
-    print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    print header 
+    #print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+    #print header 
     return table, header
 
 table1, header1 = ExtractHeader(table1)
-table2, header2 = ExtractHeader(table2)
-
+#table2, header2 = ExtractHeader(table2)
+'''
 def SanityCheck(table, header):
     data_extract = table.map(lambda line: (line.split(','))) \
     .filter(lambda line: len(line) == len(header))
     #print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
     #print 'Columns number: ', data_extract.count()
     return data_extract
-
+'''
 #table1 = SanityCheck(table1, header1)
 #table2 = SanityCheck(table2, header2)
 
-data_extract = table2.map(lambda line: line[4]+line[5]+line[6])
-data_extract = table2.map(lambda line: ''.join(re.findall('\d+', line[4]+line[5]+line[6] )))
+#data_extract = table2.map(lambda line: line[4]+line[5]+line[6])
+#data_extract = table2.map(lambda line: ''.join(re.findall('\d+', line[4]+line[5]+line[6] )))
 
 #aaa = data_extract.filter(lambda line: line == '19782')
-frequencies = data_extract.map(lambda w: (w, 1)).reduceByKey(lambda v1,v2: v1+v2)
-print frequencies.take(10)
-'''
+#frequencies = data_extract.map(lambda w: (w, 1)).reduceByKey(lambda v1,v2: v1+v2)
+#print frequencies.take(10)
 
-#frequencies.saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
+data_extract = table2.map(lambda line: line[2])
+data_extract.saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
