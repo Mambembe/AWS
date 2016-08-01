ec109944239e56ec515945b349effa3240708f33
Mambembe <matteo.giaretti@gmail.com>
2016-08-01 19:02:24 +0200

    sdv

--- MyJoin.py
+++ MyJoin.py
@@ -12,8 +12,8 @@ sys.path.append('/root/spark/python/')
 
 sc = pyspark.SparkContext()
 
-#table1 = sc.textFile('s3n://bigdives3/DataClean/dbo.shop.header.droppedhard.csv')
-table2 = sc.textFile('s3n://bigdives3/DataClean/DataClean/dbo.shop.STAT_storico_dett.droppedhard.csv')
+table1 = sc.textFile('s3n://bigdives3/DataClean/dbo.shop.header.droppedhard.csv')
+#table2 = sc.textFile('s3n://bigdives3/DataClean/DataClean/dbo.shop.STAT_storico_dett.droppedhard.csv')
 
 #table1.take(10).saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
 #print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ecce !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
