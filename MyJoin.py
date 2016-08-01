fc0cdaa550ebb1a3c6c13f267b4b6310533081a6
Mambembe <matteo.giaretti@gmail.com>
2016-08-01 18:43:32 +0200

    sdv

--- MyJoin.py
+++ MyJoin.py
@@ -12,8 +12,8 @@ sys.path.append('/root/spark/python/')
 
 sc = pyspark.SparkContext()
 
-table1 = sc.textFile('s3n://bigdives3/DataClean/dbo.shop.header.droppedhard.csv')
-#table2 = sc.textFile('s3n://bigdives3/DataClean/DataClean/dbo.shop.STAT_storico_dett.droppedhard.csv').cache
+#table1 = sc.textFile('s3n://bigdives3/DataClean/dbo.shop.header.droppedhard.csv')
+table2 = sc.textFile('s3n://bigdives3/DataClean/DataClean/dbo.shop.STAT_storico_dett.droppedhard.csv').cache
 
 #table1.take(10).saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
 #print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ecce !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
@@ -29,8 +29,8 @@ def ExtractHeader(table):
     #print header 
     return table, header
 
-table1, header1 = ExtractHeader(table1)
-#table2, header2 = ExtractHeader(table2)
+#table1, header1 = ExtractHeader(table1)
+table2, header2 = ExtractHeader(table2)
 '''
 def SanityCheck(table, header):
     data_extract = table.map(lambda line: (line.split(','))) \
@@ -43,11 +43,12 @@ def SanityCheck(table, header):
 #table2 = SanityCheck(table2, header2)
 
 #data_extract = table2.map(lambda line: line[4]+line[5]+line[6])
-#data_extract = table2.map(lambda line: ''.join(re.findall('\d+', line[4]+line[5]+line[6] )))
-
+data_extract = table2.map(lambda line: ''.join(re.findall('\d+', line[4]+line[5]+line[6] )))\
+                    .map(lambda line: line[0:5])
+aaa = data_extract.filter(lambda line: line == '19782')
 #aaa = data_extract.filter(lambda line: line == '19782')
 #frequencies = data_extract.map(lambda w: (w, 1)).reduceByKey(lambda v1,v2: v1+v2)
 #print frequencies.take(10)
 
-data_extract = table1.map(lambda line: line[2])
-data_extract.saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
+#data_extract = table1.map(lambda line: line[2])
+aaa.saveAsTextFile('s3n://bigdives3/DataClean/Join_query')
